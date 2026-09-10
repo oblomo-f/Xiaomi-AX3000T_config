cc0b3fce1ecb8e1a93fbfe3131dac7d6abd2cc6d
oblomo-f <82931753+oblomo-f@users.noreply.github.com>
2026-09-11 01:45:40 +0300

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-/etc/init.d/podkop-watchdog stop 2>/dev/null
-/etc/init.d/podkop-watchdog disable 2>/dev/null
+/etc/init.d/podkop-watchdog stop >/dev/null 2>&1
+/etc/init.d/podkop-watchdog disable >/dev/null 2>&1
 rm -f /etc/init.d/podkop-watchdog /root/podkop-watchdog.sh /root/podkop-watchdog.log /root/podkop-watchdog.rotate
 echo "Podkop Watchdog removed."
