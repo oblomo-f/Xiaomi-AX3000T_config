6fb1f1a3ac8e5779865a4059e0df81a775e1a326
oblomo-f <82931753+oblomo-f@users.noreply.github.com>
2026-09-11 12:17:14 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -272,7 +272,7 @@ uninstall_watchdog() {
 while true; do
     clear
     echo "=========================================="
-    echo "     Xiaomi AX3000T — Podkop Watchdog"
+    echo "            Podkop Watchdog"
     echo "=========================================="
     echo ""
 
